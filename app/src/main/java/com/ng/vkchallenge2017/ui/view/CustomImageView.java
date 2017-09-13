64a8378613fe872b7009c6c4bdab1e3c824f0de0
Nikita Gusarov <abbath0767@gmail.com>
2017-09-13 21:57:25 +0300

    now WORK!

--- app/src/main/java/com/ng/vkchallenge2017/ui/view/CustomImageView.java
+++ app/src/main/java/com/ng/vkchallenge2017/ui/view/CustomImageView.java
@@ -21,6 +21,7 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -91,12 +92,15 @@ public class CustomImageView extends ConstraintLayout {
     }
 
     public void clear() {
-        mImageViewTop.setImageResource(android.R.color.transparent);
-        mImageViewTop.setBackgroundResource(android.R.color.transparent);
-        mImageViewMid.setImageResource(android.R.color.transparent);
-        mImageViewMid.setBackgroundResource(android.R.color.transparent);
-        mImageViewBot.setImageResource(android.R.color.transparent);
-        mImageViewBot.setBackgroundResource(android.R.color.transparent);
+//        mImageViewTop.setImageResource(android.R.color.transparent);
+//        mImageViewTop.setBackgroundResource(android.R.color.transparent);
+        mImageViewTop.setImageDrawable(null);
+//        mImageViewMid.setImageResource(android.R.color.transparent);
+//        mImageViewMid.setBackgroundResource(android.R.color.transparent);
+        mImageViewMid.setImageDrawable(null);
+//        mImageViewBot.setImageResource(android.R.color.transparent);
+//        mImageViewBot.setBackgroundResource(android.R.color.transparent);
+        mImageViewBot.setImageDrawable(null);
     }
 
     @Override
@@ -113,7 +117,7 @@ public class CustomImageView extends ConstraintLayout {
         if (mMode == PostPresenter.Mode.POST) {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-            if (realHeight > exceptedHeight) {
+            if (rest > exceptedHeight) {
 //                Timber.i("onMeasure. need SQUARABLE");
                 super.onMeasure(widthMeasureSpec, widthMeasureSpec);
             } else {
@@ -160,85 +164,59 @@ public class CustomImageView extends ConstraintLayout {
     }
 
     public void setPng(final Integer pngResId) {
-//        mImageViewMid.setImageResource(pngResId);
-        mImageViewMid.setImageBitmap(decodeSampledBitmapFromResource(mContext.getResources(), pngResId, getWidth(), getHeight()));
+        Glide.with(mContext)
+                .load(pngResId)
+                .asBitmap()
+                .into(mImageViewMid);
     }
 
     public void setAsset(final List<Integer> asset) {
-        mImageViewTop.setImageResource(asset.get(0));
-        mImageViewMid.setImageResource(asset.get(1));
-        mImageViewBot.setImageResource(asset.get(2));
 
-        Timber.i("setAsset");
+        Glide.with(mContext)
+                .load(asset.get(0))
+                .asBitmap()
+                .into(mImageViewTop);
+        Glide.with(mContext)
+                .load(asset.get(1))
+                .asBitmap()
+                .into(mImageViewMid);
+        Glide.with(mContext)
+                .load(asset.get(2))
+                .asBitmap()
+                .into(mImageViewBot);
     }
 
-    public Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                  int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    public int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) >= reqHeight
-                    && (halfWidth / inSampleSize) >= reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
 
     //todo make async!
     private int calculateTextColour() {
 
-        Bitmap midBitMap = null;
-        try {
-            Timber.i("calculateTextColour. %s", mImageViewMid.getDrawable());
-            if (mImageViewMid.getDrawable() instanceof BitmapDrawable) {
-                midBitMap = ((BitmapDrawable) mImageViewMid.getDrawable()).getBitmap();
-            } else {
-                Timber.i("calculateTextColour. %d", getHeight());
-                if (getWidth() == 0)
-                    return 0;
-                midBitMap = Bitmap.createBitmap(getWidth()/10, getHeight()/10, Bitmap.Config.RGB_565);
-                Canvas canvas = new Canvas(midBitMap);
-                mImageViewMid.draw(canvas);
-            }
+//        Bitmap midBitMap = null;
+//        try {
+//            Timber.i("calculateTextColour. %s", mImageViewMid.getDrawable());
+//            if (mImageViewMid.getDrawable() instanceof BitmapDrawable) {
+//                midBitMap = ((BitmapDrawable) mImageViewMid.getDrawable()).getBitmap();
+//            } else {
+//                Timber.i("calculateTextColour. %d", getHeight());
+//                if (getWidth() == 0)
+//                    return 0;
+//                midBitMap = Bitmap.createBitmap(getWidth()/10, getHeight()/10, Bitmap.Config.RGB_565);
+//                Canvas canvas = new Canvas(midBitMap);
+//                mImageViewMid.draw(canvas);
+//            }
+//
+//            Palette p = new Palette.Builder(midBitMap).generate();
+//
+//            Palette.Swatch swatch = p.getVibrantSwatch();
+//
+//            if (swatch == null)
+//                return 0;
+//            else
+//                return swatch.getTitleTextColor();
+//        } finally {
+//            midBitMap = null;
+//        }
 
-            Palette p = new Palette.Builder(midBitMap).generate();
-
-            Palette.Swatch swatch = p.getVibrantSwatch();
-
-            if (swatch == null)
-                return 0;
-            else
-                return swatch.getTitleTextColor();
-        } finally {
-            midBitMap = null;
-        }
+        return 0;
     }
 
     public void calculateAverageColour() {
